2c27ee01740188e01c915bfcb3266f9b0f2098e8
ldulivo <ldulivo@gmail.com>
2022-10-11 23:59:48 -0300

    CV added

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -22,12 +22,18 @@ const AboutMe = () => {
                 <h4>Desarrollador WEB <span>React.js</span> Junior.</h4>
               </header>
               <main>
-                <p>Soy informático con experiencia en redes y sistemas.</p>
+                <p>Soy informático, me divierte programar, además tengo experiencia como freelancer en redes y sistemas.</p>
+                <p>Llevo más de 2 años practicando React, Javascript, HTML y CSS.</p>
                 <p>En 2015 terminé mi formación Universitaria y recientemente completé un curso de DESARROLLO WEB-Js/NODE Js FULL STACK del Ministerio de Educación del Gobierno de la Ciudad de Buenos Aires (Argentina).</p>
-                <p>Gracias a mi trayectoria laboral he adquirido buena capacidad para analizar y resolver problemas. Me gusta el trabajo en equipo, ya que puedo aprender y enseñar.</p>
+                <p>Gracias a mi trayectoria laboral he adquirido buena capacidad para analizar y resolver problemas. Me gusta el trabajo en equipo, ya que puedo aprender de los demás y compartir mis conocimientos.</p>
               </main>
               <footer>
-                <a href="#" className="btn">Mi CV</a>
+                <a
+                  href={`${process.env.PUBLIC_URL}/static/CV_DUlivo_Leonardo_Ariel.pdf`}
+                  className="btn"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >Mi CV</a>
               </footer>
             </div>
           </div>
